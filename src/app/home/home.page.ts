ff9cd2a29ffc274da342531bf1aaef5ce3bd784b
onrush1995 <moniruzzaman.eng1995@gmail.com>
2019-02-28 12:07:44 +0200

    Walking/Resting/Running Button has been created

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { DeviceMotion, DeviceMotionAccelerationData } from '@ionic-native/device-motion/ngx';
+import {DeviceMotionAccelerometerOptions} from '@ionic-native/device-motion';
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+    x: number;
+    y: number;
+    z: number;
+    timeStamp: number;
+    id: any;
+    status: any;
+    constructor(private deviceMotion: DeviceMotion) {
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
+        this.timeStamp = 0;
+    }
+    start() {
+        console.log('working');
+        try {
+            const option: DeviceMotionAccelerometerOptions = {
+                    frequency : 200
+                };
+            this.id = this.deviceMotion.watchAcceleration(option).subscribe((acc: DeviceMotionAccelerationData) => {
+                    this.x = acc.x;
+                    this.y = acc.y;
+                    this.z = acc.z;
+                    this.timeStamp = acc.timestamp;
+                const result = (this.x * this.x + this.y * this.y + this.z * this.z) / (9 * 9);
+                console.log(result);
+
+                const newX = Math.abs(this.x);
+
+                if ( newX > 1.0 && result < 1.5 ) {
+                    this.status = 'Walking';
+                }
+                if ( newX > 1.5 ) {
+                    this.status = 'Running';
+                }
+                if ( newX < 1 ) {
+                    this.status = 'Resting';
+                }
+            });
+        } catch (err) {
+            alert('Error' + err);
+        }
+    }
+    stop() {
+        this.id.unsubscribe();
+        console.log('working');
+    }
+}
